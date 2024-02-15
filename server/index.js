868b7f09098ac7da2aaf1bbb212809324da62162
ernestoIJ <ibanezej10@gmail.com>
2024-02-15 11:13:11 -0700

    fetching error again

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://artificialgallery.tech/'
+  origin: 'https://www.artificialgallery.tech/'
 }));
 app.use(express.json({ limit: "50mb" }));
 
