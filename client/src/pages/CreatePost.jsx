aab50b7d5fb9b572b22fef7061179d04e8036ee4
ernestoIJ <ibanezej10@gmail.com>
2024-02-15 12:15:53 -0700

    turning things back to the original

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ function CreatePost() {
       if (form.prompt) {
          try {
             setGeneratingImg(true);
-            const response = await fetch("https://artificialgallery-api.vercel.app/api/v1/dalle", {
+            const response = await fetch("https://artificialgallery.onrender.com/api/v1/dalle", {
                method: "POST",
                headers: {
                   "Content-Type": "application/json",
@@ -57,7 +57,7 @@ function CreatePost() {
          setLoading(true);
 
          try {
-            const response = await fetch("https://artificialgallery-api.vercel.app/api/v1/post", {
+            const response = await fetch("https://artificialgallery.onrender.com/api/v1/post", {
                method: "POST",
                headers: {
                   "Content-Type": "application/json",
--- client/src/pages/UserLikedPosts.jsx
+++ client/src/pages/UserLikedPosts.jsx
@@ -26,7 +26,7 @@ function UserLikedPosts() {
          setLoading(true);
 
          try {
-            const response = await fetch("https://artificialgallery-api.vercel.app/api/v1/post", {
+            const response = await fetch("https://artificialgallery.onrender.com/api/v1/post", {
                method: "GET",
                headers: {
                   "Content-Type": "application/json",
