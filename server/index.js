4a93903de488a96e4c691d88a78bff481bb879de
ernestoIJ <ibanezej10@gmail.com>
2024-02-15 12:17:14 -0700

    took off cors origin

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://artificialgallery-socialmedia.vercel.app'
-}));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
